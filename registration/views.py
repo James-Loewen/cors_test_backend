f64611b18d6acd51a6f6aa516873f952cc3acede
James Loewen <jamesfloewen@gmail.com>
2023-07-11 15:38:10 -0400

    Add registration app

---
+++ registration/views.py
@@ -0,0 +1,42 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import AnonymousUser, User
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Create your views here.
+class MyLoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username', None)
+        password = request.data.get('password', None)
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return Response({"message": "Successfully logged in!"})
+        
+        return Response({"message": "Invalid credentials"})
+
+
+class MyLogoutView(APIView):
+    def post(self, request):
+        if type(request.user) is not AnonymousUser:
+            logout(request)
+            return Response({"message": "Successfully logged out!"})
+        
+        return Response({"message": "A user was not logged in."})
+
+
+class RegisterUser(APIView):
+    def post(self, request):
+        try:
+            username = request.data["username"]
+            password = request.data["password"]
+
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+
+            return Response({"message": "User successfully created!"})
+        except KeyError:
+            return Response({"message": "missing or invalid input"})
