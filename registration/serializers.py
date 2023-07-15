577748911ab5c19c53e0c2fc6c3312236a32a17c
James Loewen <jamesfloewen@gmail.com>
2023-07-15 14:23:40 -0400

    Update User serializer .create() method
    Also attempt to update delete_users script

--- registration/serializers.py
+++ registration/serializers.py
@@ -6,4 +6,7 @@ from rest_framework.serializers import ModelSerializer
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "first_name", "last_name", "username"]
+        fields = ["id", "first_name", "last_name", "username", "password"]
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
--- scripts/delete_users.py
+++ scripts/delete_users.py
@@ -1,19 +1,29 @@
 #!/home/pynoodler/.virtualenvs/cors_venv/bin/python
 
-# DJANGO PROJECT SETUP
 import sys
 import os
 from pathlib import Path
 
-import django
+try:
+    import django
+    from django.contrib.auth.models import User
+    from django.core.exceptions import ObjectDoesNotExist, ImproperlyConfigured
+except ImportError:
+    sys.exit("Django is not installed.")
 
+# DJANGO PROJECT SETUP
 path = Path(__file__).resolve().parent.parent
 sys.path.append(path.as_posix())
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cors_test_backend.settings")
-django.setup()
+
+try:
+    django.setup()
+except ImproperlyConfigured:
+    sys.exit("Django settings are incorrect or not found.")
 
 # SCRIPT START
-from django.contrib.auth.models import User
-
-all_non_su = User.objects.filter(is_superuser=False)
-all_non_su.delete()
+try:
+    all_non_su = User.objects.filter(is_superuser=False)
+    all_non_su.delete()
+except ObjectDoesNotExist:
+    print("No non-superuser users found.")
